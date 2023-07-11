e05f39a94f405e263dfa253e0389ad3dcb5eb637
Braulio Lledo <brauliolledo@gmail.com>
2023-07-11 16:52:16 -0400

    feat: write basic entry composer and entry manager

---
+++ src/components/entry-composer.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import EntryManager from '../api/EntryManager';
+
+	let entryBody: string = ``;
+
+	const handleAddEntry = () => {
+		EntryManager.createEntry({
+			body: entryBody
+		});
+	};
+</script>
+
+<div class="flex flex-row self-end w-full h-12 m-4">
+	<textarea
+		bind:value={entryBody}
+		class="textarea textarea-bordered flex-grow"
+		placeholder="Start by writing an entry..."
+	/>
+	<button class="btn btn-primary ml-4" on:click={handleAddEntry}>ADD</button>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,19 @@
-<button class="btn">Button</button>
+<script lang="ts">
+	import { browser } from '$app/environment';
+	import { QueryClient, QueryClientProvider } from '@tanstack/svelte-query';
+	import EntryComposer from '../components/entry-composer.svelte';
+
+	const queryClient = new QueryClient({
+		defaultOptions: {
+			queries: {
+				enabled: browser
+			}
+		}
+	});
+</script>
+
+<QueryClientProvider client={queryClient}>
+	<div class="h-screen flex flex-row">
+		<EntryComposer />
+	</div>
+</QueryClientProvider>
